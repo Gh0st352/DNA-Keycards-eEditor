3c5ede3a0333a1f6cabe0d429730a14fccc5e496
Gh0st352 <matthew.a.mirarchi@gmail.com>
2023-05-27 14:13:32 -0600

    pre

--- Classes/GenerateConfigs.vb
+++ Classes/GenerateConfigs.vb
@@ -7,125 +7,56 @@
 'Imports Syncfusion.Windows.Tools.Controls
 
 'Namespace Classes
-'    Public Class GenerateConfigs
-'        Public Class Weapons
+Imports System.Collections.ObjectModel
 
-'            Public Shared Async Function GenerateConfig(t_tier As String) As Task
-'                ExportTypesCollection(GlobalVariables.Types.Types, "KeyCard_Weapons_Config.json", t_tier)
+Public Class GenerateConfigs
+    Public Class Weapons
+        Public Class WeaponInfo
+            Public Property dna_Tier As String
+            Public Property dna_WeaponCategory As String = "main"
+            Public Property dna_TheChosenOne As String = ""
+            Public Property dna_Magazine As String = "random"
+            Public Property dna_Ammunition As String = "random"
+            Public Property dna_OpticType As String = "random"
+            Public Property dna_Suppressor As String = "random"
+            Public Property dna_UnderBarrel As String = "random"
+            Public Property dna_ButtStock As String = "random"
+            Public Property dna_HandGuard As String = "random"
+            Public Property dna_Wrap As String = "random"
+        End Class
+
+        Public Shared RedWeaponKits As ObservableCollection(Of WeaponInfo)
+        Public Shared PurpleWeaponKits As ObservableCollection(Of WeaponInfo)
+        Public Shared BlueWeaponKits As ObservableCollection(Of WeaponInfo)
+        Public Shared GreenWeaponKits As ObservableCollection(Of WeaponInfo)
+        Public Shared YellowWeaponKits As ObservableCollection(Of WeaponInfo)
+    End Class
+
+    Public Class Clothes
+        Public Class ClothesInfo
+            Public Property dna_Tier As String
+            Public Property dna_Helm As String = ""
+            Public Property dna_Shirt As String = ""
+            Public Property dna_Vest As String = ""
+            Public Property dna_Pants As String = ""
+            Public Property dna_Shoes As String = ""
+            Public Property dna_Backpack As String = ""
+            Public Property dna_Gloves As String = ""
+            Public Property dna_Belt As String = ""
+            Public Property dna_Facewear As String = ""
+            Public Property dna_Eyewear As String = ""
+            Public Property dna_Armband As String = ""
+            Public Property dna_NVG As String = ""
+        End Class
+
+        Public Shared RedClothesKits As ObservableCollection(Of ClothesInfo)
+        Public Shared PurpleClothesKits As ObservableCollection(Of ClothesInfo)
+        Public Shared BlueClothesKits As ObservableCollection(Of ClothesInfo)
+        Public Shared GreenClothesKits As ObservableCollection(Of ClothesInfo)
+        Public Shared YellowClothesKits As ObservableCollection(Of ClothesInfo)
+    End Class
 
 
-'            End Function
 
-
-'            Public Class WeaponInfo
-'                Public Property dna_Tier As String
-'                Public Property dna_WeaponCategory As String = "main"
-'                Public Property dna_TheChosenOne As String = ""
-'                Public Property dna_Magazine As String = "random"
-'                Public Property dna_Ammunition As String = "random"
-'                Public Property dna_OpticType As String = "random"
-'                Public Property dna_Suppressor As String = "random"
-'                Public Property dna_UnderBarrel As String = "random"
-'                Public Property dna_ButtStock As String = "random"
-'                Public Property dna_HandGuard As String = "random"
-'                Public Property dna_Wrap As String = "random"
-'            End Class
-'            Public Shared RedWeaponKits As ObservableCollection(Of WeaponInfo)
-'            Public Shared PurpleWeaponKits As ObservableCollection(Of WeaponInfo)
-'            Public Shared BlueWeaponKits As ObservableCollection(Of WeaponInfo)
-'            Public Shared GreenWeaponKits As ObservableCollection(Of WeaponInfo)
-'            Public Shared YellowWeaponKits As ObservableCollection(Of WeaponInfo)
-
-'            Public Shared Function StringExistsInCollection(ByVal searchString As String, ByVal collection As ObservableCollection(Of String)) As Boolean
-'                Return collection.Any(Function(item) String.Equals(item, searchString, StringComparison.OrdinalIgnoreCase))
-'            End Function
-
-'            Public Shared Async Sub ExportTypesCollection(t_Types As ObservableCollection(Of GlobalVariables.Types.TypeInfo), fileName As String, t_tier As String)
-'                Dim weaponList As New List(Of WeaponInfo)()
-
-'                For Each type In t_Types
-
-
-'                    ''''''''FLAGS
-
-'                    'checkedAddArgs = Await determineIfAdd(type)
-
-
-'                    Dim workerThread As New Threading.Thread(AddressOf MainWindow.determineIfAdd)
-'                    workerThread.Start()
-
-
-'                    'Dim weapon As New WeaponInfo()
-'                    'weapon.dna_Tier = t_tier
-'                    'weapon.dna_TheChosenOne = type.typename
-'                    'Dim exists As Boolean = StringExistsInCollection(type.typename, GlobalVariables.SideArms)
-'                    'If exists Then
-'                    '    weapon.dna_WeaponCategory = "side"
-'                    'End If
-
-'                    'CHECK IF TYPE CONTAINS REQUIRED FLAGS FOR ADD
-'                    'if selectedcats is not nothing then
-'                    'for every selectedcat in selectedcats
-'                    ' if type.section contains selected cat then
-'                    'flag to add
-'                    'else continue
-
-'                    If checkedAddArgs = True Then
-'                        weaponList.Add(weapon)
-'                    End If
-'                Next
-
-'                'Dim json = JsonConvert.SerializeObject(New With {Key .m_DNAConfig_Weapons = weaponList}, Formatting.Indented)
-'                '' Get the directory path of the executable
-'                'Dim executableDirectory = AppDomain.CurrentDomain.BaseDirectory
-
-'                '' Combine the directory path with the provided file name
-'                'Dim filePath = Path.Combine(executableDirectory, fileName)
-
-'                '' Write the JSON to the file
-'                'File.WriteAllText(filePath, json)
-'                ''File.WriteAllText(filePath, json)
-'            End Sub
-'            'Public Shared Sub ExportTypesToJson(t_Types As ObservableCollection(Of GlobalVariables.Types.TypeInfo), fileName As String, t_tier As String)
-'            '    Dim weaponList As New List(Of WeaponInfo)()
-
-'            '    For Each type In t_Types
-'            '        Dim weapon As New WeaponInfo()
-'            '        weapon.dna_Tier = t_tier
-'            '        weapon.dna_TheChosenOne = type.typename
-'            '        Dim exists As Boolean = StringExistsInCollection(type.typename, GlobalVariables.SideArms)
-'            '        If exists Then
-'            '            weapon.dna_WeaponCategory = "side"
-'            '        End If
-
-'            '        'CHECK IF TYPE CONTAINS REQUIRED FLAGS FOR ADD
-'            '        'if selectedcats is not nothing then
-'            '        'for every selectedcat in selectedcats
-'            '        ' if type.section contains selected cat then
-'            '        'flag to add
-'            '        'else continue
-
-'            '        If checkedAddArgs = True Then
-'            '            weaponList.Add(weapon)
-'            '        End If
-'            '    Next
-
-'            '    Dim json = JsonConvert.SerializeObject(New With {Key .m_DNAConfig_Weapons = weaponList}, Formatting.Indented)
-'            '    ' Get the directory path of the executable
-'            '    Dim executableDirectory = AppDomain.CurrentDomain.BaseDirectory
-
-'            '    ' Combine the directory path with the provided file name
-'            '    Dim filePath = Path.Combine(executableDirectory, fileName)
-
-'            '    ' Write the JSON to the file
-'            '    File.WriteAllText(filePath, json)
-'            '    'File.WriteAllText(filePath, json)
-'            'End Sub
-'            'Public Function GetWeaponKitByCategory(category As String) As GlobalVariables.Types.WeaponKitSettings
-'            '    Dim selectedKit = GlobalVariables.WeaponKits.FirstOrDefault(Function(kit) kit.Label.Contains(category))
-'            '    Return selectedKit
-'            'End Function
-
-'        End Class
-'    End Class
+End Class
 'End Namespace
--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -397,6 +397,7 @@ Partial Public Class MainWindow
 
             For Each tSlot As ObservableCollection(Of String) In tClothingParts
                 If IsNothing(tSlot) Then
+                    Continue For
                 Else
 
 
@@ -757,56 +758,56 @@ Partial Public Class MainWindow
         Return collection.Any(Function(item) String.Equals(item, searchString, StringComparison.OrdinalIgnoreCase))
     End Function
 
-    Public Class GenerateConfigs
-        Public Class Weapons
-            Public Class WeaponInfo
-                Public Property dna_Tier As String
-                Public Property dna_WeaponCategory As String = "main"
-                Public Property dna_TheChosenOne As String = ""
-                Public Property dna_Magazine As String = "random"
-                Public Property dna_Ammunition As String = "random"
-                Public Property dna_OpticType As String = "random"
-                Public Property dna_Suppressor As String = "random"
-                Public Property dna_UnderBarrel As String = "random"
-                Public Property dna_ButtStock As String = "random"
-                Public Property dna_HandGuard As String = "random"
-                Public Property dna_Wrap As String = "random"
-            End Class
+    'Public Class GenerateConfigs
+    '    Public Class Weapons
+    '        Public Class WeaponInfo
+    '            Public Property dna_Tier As String
+    '            Public Property dna_WeaponCategory As String = "main"
+    '            Public Property dna_TheChosenOne As String = ""
+    '            Public Property dna_Magazine As String = "random"
+    '            Public Property dna_Ammunition As String = "random"
+    '            Public Property dna_OpticType As String = "random"
+    '            Public Property dna_Suppressor As String = "random"
+    '            Public Property dna_UnderBarrel As String = "random"
+    '            Public Property dna_ButtStock As String = "random"
+    '            Public Property dna_HandGuard As String = "random"
+    '            Public Property dna_Wrap As String = "random"
+    '        End Class
 
-            Public Shared RedWeaponKits As ObservableCollection(Of WeaponInfo)
-            Public Shared PurpleWeaponKits As ObservableCollection(Of WeaponInfo)
-            Public Shared BlueWeaponKits As ObservableCollection(Of WeaponInfo)
-            Public Shared GreenWeaponKits As ObservableCollection(Of WeaponInfo)
-            Public Shared YellowWeaponKits As ObservableCollection(Of WeaponInfo)
-        End Class
+    '        Public Shared RedWeaponKits As ObservableCollection(Of WeaponInfo)
+    '        Public Shared PurpleWeaponKits As ObservableCollection(Of WeaponInfo)
+    '        Public Shared BlueWeaponKits As ObservableCollection(Of WeaponInfo)
+    '        Public Shared GreenWeaponKits As ObservableCollection(Of WeaponInfo)
+    '        Public Shared YellowWeaponKits As ObservableCollection(Of WeaponInfo)
+    '    End Class
 
-        Public Class Clothes
-            Public Class ClothesInfo
-                Public Property dna_Tier As String
-                Public Property dna_Helm As String = ""
-                Public Property dna_Shirt As String = ""
-                Public Property dna_Vest As String = ""
-                Public Property dna_Pants As String = ""
-                Public Property dna_Shoes As String = ""
-                Public Property dna_Backpack As String = ""
-                Public Property dna_Gloves As String = ""
-                Public Property dna_Belt As String = ""
-                Public Property dna_Facewear As String = ""
-                Public Property dna_Eyewear As String = ""
-                Public Property dna_Armband As String = ""
-                Public Property dna_NVG As String = ""
-            End Class
+    '    Public Class Clothes
+    '        Public Class ClothesInfo
+    '            Public Property dna_Tier As String
+    '            Public Property dna_Helm As String = ""
+    '            Public Property dna_Shirt As String = ""
+    '            Public Property dna_Vest As String = ""
+    '            Public Property dna_Pants As String = ""
+    '            Public Property dna_Shoes As String = ""
+    '            Public Property dna_Backpack As String = ""
+    '            Public Property dna_Gloves As String = ""
+    '            Public Property dna_Belt As String = ""
+    '            Public Property dna_Facewear As String = ""
+    '            Public Property dna_Eyewear As String = ""
+    '            Public Property dna_Armband As String = ""
+    '            Public Property dna_NVG As String = ""
+    '        End Class
 
-            Public Shared RedClothesKits As ObservableCollection(Of ClothesInfo)
-            Public Shared PurpleClothesKits As ObservableCollection(Of ClothesInfo)
-            Public Shared BlueClothesKits As ObservableCollection(Of ClothesInfo)
-            Public Shared GreenClothesKits As ObservableCollection(Of ClothesInfo)
-            Public Shared YellowClothesKits As ObservableCollection(Of ClothesInfo)
-        End Class
+    '        Public Shared RedClothesKits As ObservableCollection(Of ClothesInfo)
+    '        Public Shared PurpleClothesKits As ObservableCollection(Of ClothesInfo)
+    '        Public Shared BlueClothesKits As ObservableCollection(Of ClothesInfo)
+    '        Public Shared GreenClothesKits As ObservableCollection(Of ClothesInfo)
+    '        Public Shared YellowClothesKits As ObservableCollection(Of ClothesInfo)
+    '    End Class
 
 
 
-    End Class
+    'End Class
     ' Function to find the index of the child TreeViewNode with content containing the specified string.
     Private Function FindChildIndex(parentNode As TreeViewNode, searchString As String) As Integer
         ' Check if the parent node exists and has children.
