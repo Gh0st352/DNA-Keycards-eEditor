156e034f3e3f44ca0d218af7e4a0d5782f94bf6c
Gh0st352 <matthew.a.mirarchi@gmail.com>
2023-05-28 01:24:25 -0600

    PreRelease

--- Classes/GenerateConfigs.vb
+++ Classes/GenerateConfigs.vb
@@ -3,18 +3,23 @@
 '-- 
 '-- This Is free software, And you are welcome to redistribute it with certain conditions.
 '-- 
-'-- --------------------- GNU General Public License v3.0 ------------------------------------
-'-- 
-'-- This program Is free software: you can redistribute it And/Or modify it under the terms 
-'-- of the GNU General Public License as published by the Free Software Foundation, 
-'-- Version 3 of the License.
+'-- --------------------- Creative Commons Attribution-NonCommercial-ShareAlike 3.0 IGO License ------------------------------------
+'-
+'--This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 IGO License.
+'-To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/igo/
+'
+'or
+'
+'-send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+'
 '--
 '-- This program Is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 '-- without even the implied warranty of MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.
-'-- See the GNU General Public License for more details.
 '--
-'-- You should have received a copy of the GNU General Public License in the downloaded 
-'-- program folder.  If Not, see https://www.gnu.org/licenses/.
+'-Any person or entity obtaining any Syncfusion code, licensed assemblies, Or dependencies As a result Of the Open Source Project must
+'-obtain their own licensed copy Of the Licensed Product from Syncfusion.
+'--
+'-- You should have received a copy of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 IGO License in the downloaded program folder. 
 '--
 '-- Copyright (C) 2023 Gh0st - This program comes with ABSOLUTELY NO WARRANTY
 '-- ------------------------------------------------------------------------------------------
